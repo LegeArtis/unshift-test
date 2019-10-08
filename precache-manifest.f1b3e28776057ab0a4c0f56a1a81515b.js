36b44e3c5828380e32fc3564f6672e07617b8f45
JS <jasonswonABK@gmail.com>
2019-10-08 21:06:48 +0300

    Updates

---
+++ precache-manifest.f1b3e28776057ab0a4c0f56a1a81515b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563ee693a22b6ddfa547541045e8aa03",
+    "url": "/unshift-test/index.html"
+  },
+  {
+    "revision": "e1814cd304971cb8daed",
+    "url": "/unshift-test/static/css/main.aa868d95.chunk.css"
+  },
+  {
+    "revision": "7b4593724e9ca7a2e65c",
+    "url": "/unshift-test/static/js/2.d64523ea.chunk.js"
+  },
+  {
+    "revision": "e1814cd304971cb8daed",
+    "url": "/unshift-test/static/js/main.c511a812.chunk.js"
+  },
+  {
+    "revision": "e41d010142a87b6b4df1",
+    "url": "/unshift-test/static/js/runtime-main.8a99b872.js"
+  }
+]);
